7e3e710cae43b1280474db302f89f71729891163
thsehddms-qudtls <sondongeun090814@gmail.com>
2025-08-25 16:30:31 +0900

    8월25일

---
+++ chap02/src/sec03/exam03/ByteOperationExample.java
@@ -0,0 +1,17 @@
+package sec03.exam03;
+
+public class ByteOperationExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte result = 10 + 20;
+		System.out.println(result);
+		
+		byte x = 10;
+		byte y = 20;
+		//byte result = x + y ; // 에러
+		int result2 = x + y;
+		System.out.println(result2);
+	}
+
+}
