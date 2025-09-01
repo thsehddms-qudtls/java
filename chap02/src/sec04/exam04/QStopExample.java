143da338ef3785e26ed31e20fb4e4b1c21a77451
thsehddms-qudtls <sondongeun090814@gmail.com>
2025-09-01 15:20:42 +0900

    9월1일

--- chap02/src/sec04/exam04/QStopExample.java
+++ chap02/src/sec04/exam04/QStopExample.java
@@ -2,9 +2,17 @@ package sec04.exam04;
 
 public class QStopExample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-
+		int keyCode;
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+			if(keyCode == 113) {
+				break;			//keyCode가 113일 경우(소문자 q) while을 중지함
+			}
+		}
+		System.out.println("종요합니다.");
 	}
 
 }
