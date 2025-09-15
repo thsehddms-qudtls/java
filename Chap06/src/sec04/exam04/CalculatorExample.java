e75bb81b3058f458732a392e18dedf9d3d59404a
thsehddms-qudtls <sondongeun090814@gmail.com>
2025-09-15 16:17:39 +0900

    25/09/15/월

---
+++ Chap06/src/sec04/exam04/CalculatorExample.java
@@ -0,0 +1,11 @@
+package sec04.exam04;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator myCalc = new Calculator();
+		myCalc.execute();
+	}
+
+}
